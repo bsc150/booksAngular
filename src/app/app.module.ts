baac5238de56fcd09c084ab2921b82473ed6c9c7
Nadav <bsc150@gmail.com>
2021-08-03 01:09:44 +0300

    first operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllBooksComponent } from './all-books/all-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllBooksComponent
   ],
   imports: [
     BrowserModule
